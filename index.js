676bf4b037688c681f397716dc8c6721c3c9f91e
ItsOrius <thegameradmin@icloud.com>
2022-09-27 19:31:55 -0400

    added dotenv for local testing

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config()
+
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const Sequelize = require('sequelize');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] });
 
 client.once('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
@@ -9,9 +11,6 @@ client.once('ready', () => {
 
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isChatInputCommand()) return;
-
-	const { commandName } = interaction;
-  console.log(commandName);
 });
 
 client.login(process.env["TOKEN"]);
